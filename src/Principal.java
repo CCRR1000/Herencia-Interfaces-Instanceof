af0df86107c9d489513b93c00550581f5bbb799b
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-24 02:32:15 -0600

    Se inicioó con el Menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,24 @@
 
 package src;
 
+import java.util.Scanner;
+
 import src.vehiculos.*;
 
 public class Principal {
+
     public static void main(String[] args) {
 
         Principal p = new Principal();
     }
 
-    public Principal() {
+    private Scanner scan = new Scanner(System.in);
 
-        Vehiculo[] autos = {new Carga(), new Carga(), new Carga(),new Carga(),
-            new Carga(), new Carga(), new Carrera(), new Carrera(), 
-            new Carrera(),  new Carrera(),  new Carrera(),  new Carrera(),
-            new Camioneta(), new Camioneta(), new Camioneta(), new Camioneta(),
-            new Camioneta(), new Camioneta(), new Carga(), new Carrera()};
+    Vehiculo[] autos = { new Carga(), new Carga(), new Carga(), new Carga(), new Carga(), new Carga(), new Carrera(),
+            new Carrera(), new Carrera(), new Carrera(), new Carrera(), new Carrera(), new Camioneta(), new Camioneta(),
+            new Camioneta(), new Camioneta(), new Camioneta(), new Camioneta(), new Carga(), new Carrera() };
+
+    public Principal() {
 
         imprimirAutos(autos);
 
@@ -26,18 +29,38 @@ public class Principal {
         System.out.println("\n\n  VEHICULOS");
         System.out.println("\n  MENU:");
         System.out.println("\n  1. Mostrar Autos");
-        System.out.println(  "  2. Utilidades Autos");
+        System.out.println("  2. Mover Autos");
+        System.out.println("  3. Ver Autos por Categoria Especifica");
+        int opcion = Integer.parseInt(scan.nextLine());
+
+        switch (opcion) {
+            case 1:
+                imprimirAutos(autos);
+                break;
+            case 2:
+                // moverAutos(autos);
+                break;
+            case 3:
+                // ver por categoria Autos(autos);
+                break;
+            case 4:
+                System.out.println("\n  Presentacion finalizada");
+                break;
+            default:
+                System.out.println("\n  Opcion incorrecta.");
+                break;
+        }
 
     }
 
     public String tipoInstancia(Vehiculo auto) {
 
         if (auto instanceof Camioneta) {
-            return " es una camioneta";
+            return "es una CAMIONETA";
         } else if (auto instanceof Carga) {
-            return " es un vehiculo de carga";
+            return "es un vehiculo de CARGA";
         } else {
-            return " es un auto de carreras";
+            return "es un auto de CARRERAS";
         }
 
     }
@@ -45,9 +68,9 @@ public class Principal {
     public void imprimirAutos(Vehiculo[] autos) {
 
         for (int i = 0; i < autos.length; i++) {
-            System.out.print("\n  El auto " + (i+1) + tipoInstancia(autos[i]));
+            System.out.print("\n  Auto " + (i + 1) + " - " + tipoInstancia(autos[i]));
             System.out.println(autos[i].toString());
-            
+
         }
     }
 
